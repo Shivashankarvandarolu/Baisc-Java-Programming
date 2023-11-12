dfca3ed0b9db3ef723c77ebf53099de19b12affd
Shiva Shankar Goud <147647296+Shivashankarvandarolu@users.noreply.github.com>
2023-11-12 09:35:42 +0530

    Update Print_the_max_number_with_index_array.java

--- CoreJava_Programes/src/main/java/JavaPrograms/Print_the_max_number_with_index_array.java
+++ CoreJava_Programes/src/main/java/JavaPrograms/Print_the_max_number_with_index_array.java
@@ -3,12 +3,14 @@ package JavaPrograms;
 public class Print_the_max_number_with_index_array {
 	
 	public static void main(String[] args) {
-	
+
+		// initilizing the array index with a
 	int[] a = new int[] {4,6,2,8,9,1,4,7,9,2,12,0,2};
-	
+
+		// To store the max value  with index
 	int max = a[0];
 	int index = 0 ;
-	
+	// Applyinf for loop for itertaing the loop
 	for(int i=0 ; i < a.length ; i++)
 	{
 		if(max < a[i])
@@ -17,6 +19,7 @@ public class Print_the_max_number_with_index_array {
 			index = i;
 		}
 	}
+		// print the result
 	System.out.println("max =" + max);
 	System.out.println("index =" + index);
 
