4e358df84a95ff7cb2005295cd4cb7bd26927cd0
Shiva Shankar Goud <147647296+Shivashankarvandarolu@users.noreply.github.com>
2023-11-12 09:37:19 +0530

    Update ReverseTheEachWordInAString.java

--- CoreJava_Programes/src/main/java/JavaPrograms/ReverseTheEachWordInAString.java
+++ CoreJava_Programes/src/main/java/JavaPrograms/ReverseTheEachWordInAString.java
@@ -5,20 +5,27 @@ import java.util.Scanner;
 public class ReverseTheEachWordInAString {
 
 	public static void main(String[] args) {
-		
+
+		// Scanner class helps to taking the input from the console
 		Scanner obj = new Scanner(System.in);
 		System.out.println("Enter Sentence");
-		
+
+		// Initilizing the string value inside a variable
 		String a = obj.nextLine();
-		
+
+		// Split the each word with space
 		String[] split = a.split(" ");
-		
+
+		// Store the rev string
 		String rev="";
-		
+
+		// Iterate the loop
 		for(int i=split.length-1 ; i>=0 ; i--)
 		{
 			rev = rev + split[i]+ " ";
 		}
+
+		// Print the result 
 		System.out.println(rev);
 	}
 }
