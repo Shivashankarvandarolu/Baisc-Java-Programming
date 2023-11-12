7cba1c0c8906b033b9e35ca73884563d3a48f7a7
Shiva Shankar Goud <147647296+Shivashankarvandarolu@users.noreply.github.com>
2023-11-12 10:54:15 +0530

    Update SwapTwoNumber_using_thirdVariable.java

--- CoreJava_Programes/src/main/java/JavaPrograms/SwapTwoNumber_using_thirdVariable.java
+++ CoreJava_Programes/src/main/java/JavaPrograms/SwapTwoNumber_using_thirdVariable.java
@@ -3,13 +3,15 @@ package JavaPrograms;
 import java.util.Scanner;
 
 public class SwapTwoNumber_using_thirdVariable {
-	
+	// Creating Class level static variable for that no need to create an object direct we use them
 	public static int c;
 	public static void main(String[] args) {
-		
+
+		// Scanner class helps to take the input from the console
 		Scanner obj = new Scanner(System.in);
 		System.out.println("Enter value of a and b");
-		
+
+		// Initilizing variables a,b 
 		int a = obj.nextInt();
 		int b = obj.nextInt();
 	
